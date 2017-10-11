59e4395242e49b44360dc5a9a3023ed815bfa92b
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 12:20:03 -0400

    Initial Commit

---
+++ SWFT-PRG-02-19-Annual-High-Temperatures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-19-Annual-High-Temperatures
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
